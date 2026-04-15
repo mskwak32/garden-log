5bd1914ef9f2700180130e0b644b9e5359f58290
mskwak <kase1@naver.com>
2026-04-15 16:09:15 +0900

    chore: 버전 코드 및 버전 이름 업데이트 (13 → 14, 2.4 → 2.5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.mskwak.gardendailylog"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 13
-        versionName = "2.4"
+        versionCode = 14
+        versionName = "2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         base.archivesName = "${rootProject.name} $versionName($versionCode)"
